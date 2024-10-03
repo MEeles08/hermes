8ca924e5d768ad3339fb260504226c8256dcc0e4
MEeles08 <eelesmatthew01@gmail.com>
2024-10-03 20:21:15 +0100

    Refactor JWT middleware to selected routes

--- backend/app/routes/blog.js
+++ backend/app/routes/blog.js
@@ -1,31 +1,28 @@
 module.exports = app => {
     const blogs = require("../controllers/blog.js");
-  
     var router = require("express").Router();
-
     const { verifyToken } = require("../middleware/auth.js")
   
     // Create a new Blog
-    router.post("/", blogs.create);
+    router.post("/", verifyToken, blogs.create);
   
     // Retrieve all Blogs
-    router.get("/", blogs.findAll);
+    router.get("/", verifyToken, blogs.findAll);
   
     // Retrieve all published Blogs
-    router.get("/published", blogs.findAllPublished);
+    router.get("/published", verifyToken, blogs.findAllPublished);
   
     // Retrieve a single Blog with id
-    router.get("/:id", blogs.findOne);
+    router.get("/:id",verifyToken, blogs.findOne);
   
     // Update a Blog with id
-    router.put("/:id", blogs.update);
+    router.put("/:id", verifyToken, blogs.update);
   
     // Delete a Blog with id
-    router.delete("/:id", blogs.delete);
+    router.delete("/:id", verifyToken, blogs.delete);
   
     // Delete all Blogs
-    router.delete("/", blogs.deleteAll);
+    router.delete("/", verifyToken, blogs.deleteAll);
   
-    app.use(verifyToken);
     app.use('/api/blogs', router);
   };
