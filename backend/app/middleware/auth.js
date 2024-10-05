783f73abd37f3630ae883de63925c7300517c394
MEeles08 <eelesmatthew01@gmail.com>
2024-10-05 20:56:58 +0100

    Remove console.log in auth middleware

--- backend/app/middleware/auth.js
+++ backend/app/middleware/auth.js
@@ -10,11 +10,10 @@ middlewareObj.verifyToken = (req, res, next) => {
         console.log('No JWT token found');
         return res.status(403).send('A token is required for authentication');
     }
-  
+    
     try {
         const tokenWithoutBearer = token.split(" ")[1];
         const decoded = jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET);
-        console.log('JWT Token verified');
     } catch (err) {
         console.log('Invalid JWT token found');
         return res.status(401).send('Invalid Token');
