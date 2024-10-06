04b3f55228ae6ed8e6b081e7120443f2fa301bfa
MEeles08 <eelesmatthew01@gmail.com>
2024-10-06 15:41:59 +0100

    Refactor sanity findAll query

--- backend/app/controllers/sanity.js
+++ backend/app/controllers/sanity.js
@@ -3,7 +3,9 @@ const { Sanity } = require('../utils/sanity');
 // Retrieve all Posts from Sanity.io.
 exports.findAll = async (req, res) => {
     try {
-        const query = '*[_type == "post"] | order(_createdAt desc)';
+        // const query = '*[_type == "post"] | order(_createdAt desc)';
+        const query = '*[_type == "post"] {_id, title, description, body, _createdAt, publishedAt, author-> {  _id,  name,  bio  } } | order(_createdAt desc)';
+
         const posts = await Sanity.client.fetch(query);
         res.json(posts);
     } 
