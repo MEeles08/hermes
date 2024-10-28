174d208ad354d868518f88cf12d6b6d1c05bd685
MEeles08 <eelesmatthew01@gmail.com>
2024-10-28 20:01:17 +0000

    Refactor Card component for router-link

--- frontend/src/components/elements/Card.vue
+++ frontend/src/components/elements/Card.vue
@@ -1,16 +1,17 @@
 <template>
-    <a :href="data.url" target="_blank">
-        <div class="box d-flex align-items-start my-2 shadow my-1">
-            <!-- Image on the left -->
-            <img :src="data.img" :alt="data.title">
-            <!-- Text content: title and description -->
-            <div>
-                <div class="title">{{ data.title }}</div>
-                <div class="description">
-                    {{ data.description }}
-                </div>
-            </div>
-        </div>   
+    <router-link v-if="isInternalLink(data.url)" :to="data.url" class="box d-flex align-items-start my-2 shadow my-1">
+        <img :src="data.img" :alt="data.title">
+        <div>
+            <div class="title">{{ data.title }}</div>
+            <div class="description">{{ data.description }}</div>
+        </div>
+    </router-link>
+    <a v-else :href="data.url" target="_blank" rel="noopener noreferrer" class="box d-flex align-items-start my-2 shadow my-1">
+        <img :src="data.img" :alt="data.title">
+        <div>
+            <div class="title">{{ data.title }}</div>
+            <div class="description">{{ data.description }}</div>
+        </div>
     </a>
 </template>
 
@@ -23,6 +24,11 @@
             required: true
         }
     },
+    methods: {
+        isInternalLink(url) {
+            return url.startsWith('/');
+        }
+    }
 };   
 
 </script>
